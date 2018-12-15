c974f0eadb54c368f19272ed59661235b112d520
shubham kakkar <shubham.kakkar98@gmail.com>
2018-12-15 11:01:29 +0530

    done in one go

--- App.js
+++ App.js
@@ -1,26 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import MainApp from './App/MainApp';
- 
+import MainApp from "./App/MainApp";
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
-    return (
-     
-        <MainApp />
-      
-    );
+    return <MainApp />;
   }
 }
- 
--- App/MainApp.js
+++ App/MainApp.js
@@ -1,71 +1,91 @@
-import React, {Component} from 'react';
-import {View,Text} from 'react-native';
-import {TabNavigator, TabBarTop} from 'react-navigation';
-import { Icon } from 'react-native-elements';
-import FeedPage from './Feed/FeedPage.js';
-import SearchPage from './Search/SearchPage.js';
-import AddPage from './Add/AddPage.js';
-import SavedPage from './Saved/SavedPage.js';
-import ProfilePage from './Profile/ProfilePage.js';
-export default Tabs = TabNavigator({
-	Feed:{
-	  screen: FeedPage,
-	 
-	  navigationOptions:{
-	  	
-	  	tabBarIcon: ({ tintColor }) => <Icon size={29}  name='newspaper-o'  type='font-awesome'  color='#000000'/>
-	  }
-	},
-	Search:{
-		screen: SearchPage,
-		navigationOptions:{
-	  	tabBarIcon: ({ tintColor }) => <Icon size={29}  name='search'  type='feather'  color='#000000'/>
-	  	}
-	},
-	Add:{
-		
-		screen : AddPage,
-		navigationOptions:{
-	  		tabBarIcon: ({ tintColor }) => <Icon size={38} containerStyle={{zIndex: 10000}}  name='add-circle-outline' color='#000000'/>
-	  	}
-	},
-	Saved:{
-		screen : SavedPage,
-		navigationOptions:{
-	  	tabBarIcon: ({ tintColor }) => <Icon size={29}  name='inbox'  type='feather'  color='#000000'/>
-	  	}
-	},
-	 
-	Profile:{
-		screen: ProfilePage,
+import React from "react";
+import { TabNavigator, TabBarTop } from "react-navigation";
+import { Icon } from "react-native-elements";
+import FeedPage from "./Feed/FeedPage.js";
+import SearchPage from "./Search/SearchPage.js";
+import AddPage from "./Add/AddPage.js";
+import SavedPage from "./Saved/SavedPage.js";
+import ProfilePage from "./Profile/ProfilePage.js";
 
-		navigationOptions:{
-	  	
-	  	tabBarIcon: ({ tintColor }) => <Icon size={29}  name='user-o'  type='font-awesome'  color='#000000'/>
-	  }
-	}
-},{ 
-	tabBarComponent:TabBarTop,
+const Tabs = TabNavigator(
+  {
+    Feed: {
+      screen: FeedPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            size={29}
+            name="newspaper-o"
+            type="font-awesome"
+            color="#000000"
+          />
+        )
+      }
+    },
+    Search: {
+      screen: SearchPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon size={29} name="search" type="feather" color="#000000" />
+        )
+      }
+    },
+    Add: {
+      screen: AddPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            size={38}
+            containerStyle={{ zIndex: 10000 }}
+            name="add-circle-outline"
+            color="#000000"
+          />
+        )
+      }
+    },
+    Saved: {
+      screen: SavedPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon size={29} name="inbox" type="feather" color="#000000" />
+        )
+      }
+    },
 
-	tabBarPosition: 'bottom',
-	tabBarOptions: {
-		showLabel: false,
-		showIcon : true,
-    	activeTintColor: '#000000',
-    	inactiveTintColor: '#8e8e8e',
-    	style:{
-  			elevation: 15,
-  			backgroundColor:'#fff',
-  			height: '8.5%'
-  		},
-  		iconStyle:{
-  			position: 'relative',
-  			width: '100%',
-  			height: 32
-  		},
-  		indicatorStyle:{
-  			backgroundColor:'#000'
- 		}
-  	},
-  	
-});
+    Profile: {
+      screen: ProfilePage,
+
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon size={29} name="user-o" type="font-awesome" color="#000000" />
+        )
+      }
+    }
+  },
+  {
+    tabBarComponent: TabBarTop,
+
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      showLabel: false,
+      showIcon: true,
+      activeTintColor: "#000000",
+      inactiveTintColor: "#8e8e8e",
+      style: {
+        elevation: 15,
+        backgroundColor: "#fff",
+        height: "8.5%"
+      },
+      iconStyle: {
+        position: "relative",
+        width: "100%",
+        height: 32
+      },
+      indicatorStyle: {
+        backgroundColor: "#000"
+      }
+    }
+  }
+);
+
+export default Tabs;
